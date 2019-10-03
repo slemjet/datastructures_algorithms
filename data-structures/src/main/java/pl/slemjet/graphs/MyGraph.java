369ac6ebcaa60813b274d9e4757d68d484f29a0a
SLem <slemjet@gmail.com>
2019-10-03 15:19:27 -0700

    Added sample graph with adjacentList

---
+++ data-structures/src/main/java/pl/slemjet/graphs/MyGraph.java
@@ -0,0 +1,69 @@
+package pl.slemjet.graphs;
+
+import java.util.*;
+
+public class MyGraph<T extends Comparable> {
+
+    private int numberOfNodes;
+    private Map<T, Set<T>> adjacentList;
+
+    public MyGraph() {
+        numberOfNodes = 0;
+        adjacentList = new HashMap<>();
+    }
+
+    public void addVertex(T value) {
+        if (!adjacentList.containsKey(value)) {
+            adjacentList.put(value, new HashSet<>());
+        }
+    }
+
+    public void addEdge(T value1, T value2) {
+        addNodes(value1, value2);
+        addNodes(value2, value1);
+    }
+
+    private void addNodes(T value1, T value2) {
+        if (!adjacentList.containsKey(value1)) {
+            adjacentList.put(value1, new HashSet<T>(Arrays.asList(value2)));
+        } else {
+            adjacentList.get(value1).add(value2);
+        }
+    }
+
+    public void addEdges(T value1, T... value2) {
+        for (T value : value2) {
+            addEdge(value1, value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<T, Set<T>> entry : adjacentList.entrySet()) {
+            sb.append("[ ").append(entry.getKey()).append(": ");
+            for (T value : entry.getValue()) {
+                sb.append(value).append(" ");
+            }
+            sb.append("]").append("\n");
+        }
+        return sb.toString();
+    }
+
+    class Node<T extends Comparable> implements Comparable {
+        private T value;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            return getValue().compareTo(((Node<T>) o).getValue());
+        }
+    }
+}
---
+++ data-structures/src/test/java/pl/slemjet/graphs/MyGraphTest.java
@@ -0,0 +1,38 @@
+package pl.slemjet.graphs;
+
+import org.junit.jupiter.api.Test;
+
+class MyGraphTest {
+    @Test
+    void test() {
+        // given
+        MyGraph<Integer> graph = new MyGraph<>();
+//        graph.addEdges(0, 1, 2);
+//        graph.addEdges(1, 0, 2, 3);
+//        graph.addEdges(2, 0, 1, 4);
+//        graph.addEdges(3, 1, 4);
+//        graph.addEdges(4, 2, 3, 5);
+//        graph.addEdges(5, 4, 6);
+//        graph.addEdges(6, 5);
+
+        // when
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addEdge(3, 1);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 2);
+        graph.addEdge(4, 5);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 0);
+        graph.addEdge(0, 2);
+        graph.addEdge(6, 5);
+
+        // then
+        System.out.println(graph);
+    }
+}
